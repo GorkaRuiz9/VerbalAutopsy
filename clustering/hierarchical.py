5eceed21c20b0e159743d3ef2eba92f351b7e773
GonzaloP <gonzaloperezgo@gmail.com>
2025-10-12 12:49:02 +0200

    clustering

--- clustering/hierarchical.py
+++ clustering/hierarchical.py
@@ -1,20 +1,49 @@
 # implementacion de la clase AgglomerativeClustering
 import pandas
+from utils import *
+import numpy as np
+import copy
 
 class AgglomerativeClustering:
     def __init__(self, linkage="average", metric="euclidean", minkowski_p=3):
         self.linkage = linkage
         self.metric = metric
         self.p = minkowski_p
-        self.linkage_matrix_ = []
+        self.clusters = list()
+        self.clusters_history = list()
         self.labels_ = None
 
     def fit(self, data_set: pandas.DataFrame):
         # contruye el dendograma a partir del dataset
-        while len(self.linkage_matrix_) < len(data_set):
+        self.clusters = [ClusterNode(datos=[dato], id=i) for i, dato in enumerate(data_set.values)]
+        self.clusters_history = copy.deepcopy(self.clusters)
+        
+        while len(self.clusters) > 1:
+            distancias = calcular_distancias(self.clusters, self.linkage, self.metric, self.p)
+            min_dist, (i, j) = get_min_dist(distancias)
+            clusterA = self.clusters[i]
+            clusterB = self.clusters[j]
+            self.update_clusters_list(clusterA, clusterB, min_dist)
+            
+        return None
             
             
-    
+    def update_clusters_list(self, clusterA, clusterB, min_dist):
+        new_cluster = ClusterNode(clusterA, clusterB, min_dist, clusterA.datos + clusterB.datos, len(self.clusters_history))
+        self.clusters.remove(clusterA)
+        self.clusters.remove(clusterB)
+        self.clusters.append(new_cluster)
+        self.clusters_history.append(new_cluster)
+        
     def get_clusters(self, dist_to_cut):
         
         return None
+
+
+class ClusterNode:
+    def __init__(self, left=None, right=None, distance=0.0, datos=None, id=None):
+        self.left = left          
+        self.right = right        
+        self.distance = distance  
+        self.datos = datos    
+        self.id = id              
--- clustering/utils.py
+++ clustering/utils.py
@@ -1,1 +1,20 @@
 # Para funciones complementarias
+import numpy as np
+import pandas
+from linkage import *
+import math
+
+def calcular_distancias(clusters, linkage, metric, p):
+    distancias = [[math.inf if i == j else 0.0 for i in range(len(clusters))] for j in range(len(clusters))]
+    # calcular matriz de distancias
+    for i in range(len(clusters)):
+        for j in range(i+1, len(clusters)):
+            distancias[i][j] = distancias[j][i] = inter_group_distance(clusters[i].datos, clusters[j].datos, linkage, metric, p)
+            
+    return distancias
+            
+def get_min_dist(distancias):
+    arr = np.array(distancias)
+    i, j = np.unravel_index(np.argmin(arr), arr.shape)
+    min_dist = arr[i, j]
+    return min_dist, (i, j)
